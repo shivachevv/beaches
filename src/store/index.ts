acc8e95df48f0248e055b2e745b627bfdf740041
Nikola Shivachev <nikola.shivachev@ad.mentormate.bg>
2021-12-13 19:40:15 +0200

    finished setting store and added fake test login

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from './slices/index';
 
  const store = configureStore({
-    reducer: {
-    },
+    reducer: rootReducer,
   })
 
 export default store
