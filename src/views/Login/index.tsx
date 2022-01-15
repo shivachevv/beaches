45e0495c6b4524305b17aa889fe1fa283d305eb1
Nikola Shivachev <nikola.shivachev@ad.mentormate.bg>
2022-01-15 19:16:59 +0200

    finished login page

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,34 +1,104 @@
-import { Box, Button } from "@mui/material";
+import { Alert, Box, Button, TextField } from "@mui/material";
 import * as React from "react";
-import { login, logout } from "../../store/slices/auth";
-import { useAppDispatch } from "../../store/hooks";
-import { DUMMY_PASSWORD } from "../../utils/constants";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import styles from "./index.module.scss";
+import { login } from "../../store/slices/auth";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { LoginData } from "../../interfaces";
 
 type Props = Record<string, unknown>;
+interface LoginInput {
+  email: string;
+  password: string;
+}
 
-const Navbar: React.FC = (props: Props) => {
+const Login: React.FC = (props: Props) => {
+  const { error } = useAppSelector((state) => state.auth);
+
+  const { control, handleSubmit, register, formState, setError } =
+    useForm<LoginInput>();
+  const { errors } = formState;
   const dispatch = useAppDispatch();
-  const loginUser = () => {
-    dispatch(
-      login({
-        email: "admin@gmail.com",
-        password: DUMMY_PASSWORD,
-      })
-    );
+  const loginUser: SubmitHandler<LoginInput> = (data: LoginData) => {
+    dispatch(login(data));
   };
-  const logoutUser = () => {
-    dispatch(logout());
+
+  // TODO: USE FOR REGISTER
+
+  // const emailIsUnique = async (email: string): Promise<boolean> => {
+  //   const querySnapshot = await db
+  //     .collection(DATABASE_MODELS.USERS)
+  //     .where("email", "==", email)
+  //     .get();
+  //   const [user] = querySnapshot.docs.map((doc) => doc.data());
+
+  //   return !user;
+  // };
+
+  const getEmailErrorMessage = () => {
+    if (errors?.email?.type === "required") {
+      return "Email is required!";
+    }
+    return "";
   };
+
   return (
-    <Box sx={{ mt: "70px" }}>
-      <Button variant="contained" onClick={loginUser}>
-        Login
-      </Button>
-      <Button variant="contained" onClick={logoutUser}>
-        Logout
-      </Button>
+    <Box
+      sx={{
+        mt: "100px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        width: "100%",
+      }}
+    >
+      {error && (
+        <Alert sx={{ mb: 2, width: "30%" }} severity="error">
+          {error}
+        </Alert>
+      )}
+      <form onSubmit={handleSubmit(loginUser)} className={styles.form}>
+        <Controller
+          name="email"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              type="email"
+              error={!!errors?.email}
+              helperText={getEmailErrorMessage()}
+              label="Email"
+              {...field}
+              {...register("email", {
+                required: true,
+              })}
+            />
+          )}
+        />
+        <Controller
+          name="password"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              sx={{ mt: 2 }}
+              type="password"
+              error={!!errors?.password}
+              helperText={!!errors?.password && "Password is Required"}
+              label="Password"
+              {...field}
+              {...register("password", {
+                required: true,
+              })}
+            />
+          )}
+        />
+        <Button variant="contained" type="submit" sx={{ mt: 2 }}>
+          Login
+        </Button>
+      </form>
     </Box>
   );
 };
 
-export default Navbar;
+export default Login;
