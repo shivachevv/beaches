f98cef72537bafad26ca5c473921fa8081d2db7e
Nikola Shivachev <nikola.shivachev@ad.mentormate.bg>
2022-01-24 20:17:19 +0200

    env

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,15 +2,16 @@
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import firebase from "firebase/compat";
-
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv").config();
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCb3cOdP7DQSKZnaXW9JOkxk-Y0ZHM-JLg",
-  authDomain: "beaches-e14b5.firebaseapp.com",
-  projectId: "beaches-e14b5",
-  storageBucket: "beaches-e14b5.appspot.com",
-  messagingSenderId: "198289700223",
-  appId: "1:198289700223:web:2922a05544b5b38cd4564a",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
